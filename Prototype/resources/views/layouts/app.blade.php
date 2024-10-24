33584bc480d374d736807e67f51abbcdf7f64a8b
Tribak77 <atribak34@gmail.com>
2024-10-24 09:32:59 +0100

    add the layouts

---
+++ Prototype/resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Laravel application</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <li><a href="">home</a></li>
+            <li><a href="">services</a></li>
+            <li><a href="">about</a></li>
+            <li><a href="">contact</a></li>
+        </nav>
+    </header>
+    @yield('content')
+    <footer>
+        &copy; 2024 All rights reserved.
+    </footer>
+</body>
+</html>
