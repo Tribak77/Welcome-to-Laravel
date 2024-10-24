9e411709b0500ed67bbf55784ad3267f22a278af
Tribak77 <atribak34@gmail.com>
2024-10-24 10:00:40 +0100

    add the logic in the contact controller and the route and the mail form

--- Prototype/routes/web.php
+++ Prototype/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ContactController;
+
+Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
